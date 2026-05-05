5b984b6c245b6cc5fbc12d43e0cebce961b9b65f
Noeh <noehboiser221@gmail.com>
2026-05-05 18:29:56 +0800

    vite config changes

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,18 @@
-import { defineConfig } from 'vite'
+import { defineConfig, createLogger } from 'vite'
 import react from '@vitejs/plugin-react'
 import wasm from "vite-plugin-wasm";
 import topLevelAwait from "vite-plugin-top-level-await";
-import { nodePolyfills } from 'vite-plugin-node-polyfills'; // <-- Import it here
+import { nodePolyfills } from 'vite-plugin-node-polyfills';
+
+const logger = createLogger();
+const originalWarning = logger.warn;
+logger.warn = (msg, options) => {
+    if (msg.includes('esbuild` option was specified')) return;
+    originalWarning(msg, options);
+};
 
 export default defineConfig({
+    customLogger: logger,
     plugins: [
         react(),
         wasm(),
